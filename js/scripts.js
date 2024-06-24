0bc4b0b34f805e4408b1f473d8e97303f7992ae5
jessla89 <65287769+jessla89@users.noreply.github.com>
2024-06-23 18:35:35 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,10 @@
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
+    const sideNav = document.body.querySelector('/sideNav');
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
+            target: '/sideNav',
             rootMargin: '0px 0px -40%',
         });
     };
@@ -21,7 +21,7 @@ window.addEventListener('DOMContentLoaded', event => {
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
+        document.querySelectorAll('/navbarResponsive .nav-link')
     );
     responsiveNavItems.map(function (responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
